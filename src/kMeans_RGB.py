539f9dfc8ecc674f2d30fb53340f68d4325749a0
Sanskriti Rathi <sanskriti@lawn-143-215-60-170.lawn.gatech.edu>
2019-10-31 16:46:06 -0400

    k-means code for RGB and RGB+pos
    Contains-
    1. code for both feature spaces
    2. implementation for all images for k-means RGB
    3. all the .mat files created by RGB

---
+++ src/kMeans_RGB.py
@@ -0,0 +1,76 @@
+import os
+from glob import glob
+from sklearn.cluster import KMeans
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import image
+from scipy.io import savemat
+import pickle
+
+def quantizeRGB(img_path, k):
+    origImg = image.imread(img_path).astype(np.uint8);
+    pixels = np.reshape(origImg, (-1,3)).astype('float');
+
+    kMeans = KMeans(k).fit(pixels)
+    clusterCenters = kMeans.cluster_centers_
+    labels = kMeans.labels_
+    labeledPixels = clusterCenters[labels]
+
+    outputImg = np.reshape(labeledPixels, origImg.shape).astype('uint8');    
+
+    mat_arr = np.empty((1,), dtype=np.object)
+    mat_arr[0] = outputImg
+    img_base_name = os.path.basename(img_path)
+    img_name = os.path.splitext(img_base_name)[0]
+    savemat(os.path.join(output_dir, "%s.mat" % img_name), {'segs': mat_arr})
+
+def quantizeRGBwithPos(img_path, k):
+    origImg = image.imread(img_path).astype(np.uint8);
+    dupImg = origImg[:]
+    imgShape = dupImg.shape
+
+    x,y = np.meshgrid(np.arange(0,np.size(origImg,1)),np.arange(0,np.size(origImg,0)))
+    origImgx = np.concatenate([origImg, x[...,None]], axis=2)
+    origImgy = np.concatenate([origImgx, y[...,None]], axis=2)
+    pixels = np.reshape(origImgy, [-1, 5])
+
+    kMeans = KMeans(k).fit(pixels)
+    clusterCenters = kMeans.cluster_centers_
+    labels = kMeans.labels_
+    # labeledPixels = clusterCenters[labeledPixels]
+    outputImg = np.reshape(labels, imgShape[:2]).astype('uint8')
+    mat_arr = np.empty((1,), dtype=np.object)
+    mat_arr[0] = outputImg
+    img_base_name = os.path.basename(img_path)
+    img_name = os.path.splitext(img_base_name)[0]
+    savemat(os.path.join(output_dir_pos, "%s.mat" % img_name), {'segs': mat_arr})
+
+    # segmentedImg = clusterCenters[outputImg]
+
+def main():
+    infile = open("rgb_modes_dict.pickle", "rb")
+    kDict = pickle.load(infile)
+    infile.close()
+
+    for key in kDict:
+        print("\nImage %s" % key)
+        img_path = os.path.join(test_img_dir, key)
+        quantizeRGB(img_path, kDict[key])
+
+if __name__ == '__main__':
+
+    root_dir = '..'
+    data_dir = os.path.join(root_dir, 'BSDS500', 'BSDS500', 'data')
+    test_img_dir = os.path.join(data_dir, 'images', 'test')
+    output_dir = os.path.join(root_dir, 'src', 'k_means_RGB_files')
+    output_dir_pos = os.path.join(data_dir, 'src', 'k_means_RGB_pos_files')
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    if not os.path.exists(output_dir_pos):
+        os.makedirs(output_dir_pos)
+
+    main()
+
+
