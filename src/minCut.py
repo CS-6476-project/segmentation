6cdf72a1f35d5c9d96763a593111cbd60395c3c8
pthakkar3 <pranshav.thakkar@gmail.com>
2019-12-03 00:42:08 -0500

    added minCut code
    minCut with Karger's algorithm added

---
+++ src/minCut.py
@@ -0,0 +1,69 @@
+from skimage import data, segmentation, color
+from skimage.future import graph
+from matplotlib import pyplot as plt
+import random
+import numpy as np
+
+def contract(g, v, w):
+    wNeighbors = g.neighbors(w)
+    wNeighbors = list(wNeighbors)
+    for node in wNeighbors:
+        if node != v:
+            g.add_edge(v, node)
+            vNeighbors = list(g.neighbors(v))
+        g.remove_edge(node, w)
+        if node != v:
+            g.add_edge(node, v)
+    wNeighbors = g.neighbors(w)
+    wNeighbors = list(wNeighbors)
+    if g.node[v]['labels'] == None:
+        g.node[v]['labels'] = []
+    g.node[v]['labels'] = g.node[v]['labels'] + (g.node[w]['labels'])
+    g.remove_node(w)
+
+img = data.coffee()
+
+labels1 = segmentation.slic(img, compactness=30, n_segments=400)
+
+g = graph.rag_mean_color(img, labels1, mode='similarity')
+
+while len(g) > 10:
+    v = random.choice(list(g.nodes))
+    #print(v)
+    vNeighbors = g.neighbors(v)
+    list1 = list(vNeighbors)
+    #print(list1)
+    w = random.choice((list1))
+    #print(w)
+    contract(g, v, w)
+
+print(g.nodes)
+labels = [[] for i in range(10)]
+counter = 0
+for node in g.nodes:
+    print(g.node[node])
+    labels[counter] = labels[counter] + g.node[node]['labels']
+    counter = counter + 1
+print(labels)
+
+labels2 = labels1.copy()
+print(labels2.shape)
+for x in range(labels2.shape[0]):
+    for y in range(labels2.shape[1]):
+        old = labels2[x][y]
+        for z in range(len(labels)):
+            if old in labels[z]:
+                labels2[x][y] = labels[z][0]
+
+
+#print(labels2)
+
+out2 = color.label2rgb(labels2, img, kind='avg')
+
+plt.imshow(out2)
+plt.show()
+
+
+
+
+
